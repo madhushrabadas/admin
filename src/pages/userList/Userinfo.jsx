c938ed272c01bb7c2b044698f0f6b0230d16c6cf
Tuli Das <madhushraba21@gmail.com>
2023-09-24 07:26:26 +0530

    kk

--- src/pages/userList/Userinfo.jsx
+++ src/pages/userList/Userinfo.jsx
@@ -1,97 +1,104 @@
-import * as React from "react";
 import Box from "@mui/material/Box";
 import { DataGrid } from "@mui/x-data-grid";
-import './userList.css'
+import "./userList.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { userRows } from "../../dummy/dummyData";
+import AutoFixHighSharpIcon from "@mui/icons-material/AutoFixHighSharp";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 
 const columns = [
   { field: "id", headerName: "ID", width: 90 },
   {
-    field: "firstName",
-    headerName: "First name",
-    width: 150,
-    editable: true,
+    field: "user",
+    headerName: "User",
+    width: 200,
+    renderCell: (params) => {
+      return (
+        <div className="userListUser">
+          <img className="userListImg" src={params.row.avatar} alt="" />
+          {params.row.username}
+        </div>
+      );
+    },
+  },
+  { field: "email", headerName: "Email", width: 200 },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 120,
   },
   {
-    field: "lastName",
-    headerName: "Last name",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 110,
-    editable: true,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
+    field: "transaction",
+    headerName: "Transaction Volume",
     width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
   },
-];
-
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: 'vbfgbg', age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  { id: 11, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 12, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 13, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 14, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 15, lastName: "Targaryen", firstName: "Daenerys", age: 67 },
-  { id: 16, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 17, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 18, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 19, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: 'vbfgbg', age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  { id: 11, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 12, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 13, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 14, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 15, lastName: "Targaryen", firstName: "Daenerys", age: 67 },
-  { id: 16, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 17, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 18, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 19, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  {
+    field: "action",
+    headerName: "Action",
+    width: 150,
+    renderCell: (params) => {
+      return (
+        <>
+          <Link to={"/user/" + params.row.id}>
+            <button className="userListEdit">
+              <AutoFixHighSharpIcon />
+            </button>
+          </Link>
+          <DeleteOutlineOutlinedIcon />
+          {/* <DeleteOutline
+            className="userListDelete"
+            onClick={() => handleDelete(params.row.id)}
+          /> */}
+        </>
+      );
+    },
+  },
 ];
 
 export default function Userinfo() {
+  const [data, setData] = useState(userRows);
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
   return (
     <div className="mui_table">
-      <Box sx={{  width: "100%" }}>
+      <Box sx={{ width: "100%" }}>
         <DataGrid
-          rows={rows}
+          rows={data}
+          disableSelectionOnClick
           columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: {
-                // pageSize: 5,
-              },
-            },
-          }}
-        //   pageSizeOptions={[5]}
+          pageSize={8}
           checkboxSelection
-          disableRowSelectionOnClick
         />
       </Box>
     </div>
   );
+}
+
+{
+  /*
+ 
+import { userRows } from "../../dummyData"; 
+
+export default function UserList() {
+  const [data, setData] = useState(userRows);
+
+ 
+   
+
+  return (
+    <div className="userList">
+      <DataGrid
+        rows={data}
+        disableSelectionOnClick
+        columns={columns}
+        pageSize={8}
+        checkboxSelection
+      />
+    </div>
+  );
+}
+*/
 }
